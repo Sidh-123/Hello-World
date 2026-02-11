bfc907d6e0e17344fbdde80f24568bda6c30528a
Sidh <sidhdhiyogi@gmail.com>
2026-02-10 20:49:33 -0600

    Initial commit for test_branch_1

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 class HelloWorld:
     def __init__(self):
-        self.message = "Hello, World!"
+        self.message = "Hello, World! I am here to test GitHub!"
     
     def greet(self):
         print(self.message)
+    
+    def update_message(self, new_msg):
+        self.message = new_msg
+        
 
 
 if __name__ == "__main__":
     hw = HelloWorld()
+    hw.greet()
+    hw.update_message("Hello, GitHub")
     hw.greet()
